b52af8ef8be70c0a4c7b0f4c3df02f992ee41e6b
dogarelu <dogarudanstefan@gmail.com>
2025-03-24 14:27:26 +0200

    course 3

---
+++ course3/nft-metadata.ts
@@ -0,0 +1,54 @@
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { clusterApiUrl } from "@solana/web3.js";
+import "dotenv/config";
+import { readFile } from "fs/promises";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+const umi = createUmi(clusterApiUrl("devnet"), "confirmed");
+
+const umiKeypair = umi.eddsa.createKeypairFromSecretKey(keypair.secretKey);
+const signer = createSignerFromKeypair(umi, umiKeypair);
+
+const IMAGE_URI = "https://devnet.irys.xyz/E6AGdmm6Z5vFA9sarqEurm2mEcH2Sg3t4CZvcRmCEoUu"
+
+umi.use(irysUploader());
+umi.use(signerIdentity(signer));
+
+export async function uploadMetadata() {
+    try {
+        console.log("Uploading metadata...");
+        const metadata = {
+            name: "Superliga Fantasy",
+            symbol: "SLF",
+            description: "The owner of Superliga Fantasy - 1/1",
+            image: IMAGE_URI,
+            attributes: [
+                {
+                    trait_type: "Role", value: "CTO", 
+                },
+                {
+                    trait_type: "Favourite Team", value: "FCSB",
+                },
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: IMAGE_URI,
+                    }
+                ],
+            },
+        }
+
+        const metadataUri = await umi.uploader.uploadJson(metadata);
+        console.log(`Metadata uploaded to ${metadataUri}`);
+        
+    } catch (error) {
+        
+    }
+}
+
+uploadMetadata();
